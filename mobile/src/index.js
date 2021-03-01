6be90750ffe446f79cfa21fdba2f5893bc069bf4
Jose Mauro <josemauromani@gmail.com>
2021-03-01 17:08:04 -0300

    atualizando estudos

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,41 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 import api from './services/api';
-/**
- * Nao possio valor semantico (significado)
- * Nao possue estilizacao propria 
- * Todos os componentes possuem por padrao "display: flex"
- */
+
 export default function App() {
 
     const [projects, setProjects] = useState([]);
-
     useEffect(() => {
         api.get('projects').then(response => {
             console.log(response.data);
-            setProjects(response.data);
-        })
-    }, []);
+            setProjects(response.data)
+        });
+    }, [])
 
+    async function handleAddProject() {
+        const response = await api.post('projects', {
+            title: `Novo Projeto ${Date.now()}`,
+            owner: 'Jose Mauro'
+        });
+        setProjects([...projects, response.data])
+    }
 
-    return <>
-        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-        <View style={styles.container}>
-            <Text style={styles.title}>Hello GoStack</Text>
-        </View>
-    </>
+    return (
+        <>
+            <SafeAreaView style={styles.container}>
+                <StatusBar barStyle="light-content" />
+                <FlatList
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={({ item }) => (
+                        <Text style={styles.texto}>{item.title}</Text>
+                    )}
+                />
+
+                <TouchableOpacity
+                    activeOpacity={0.6}
+                    style={styles.button}
+                    onPress={handleAddProject}
+                >
+                    <Text style={styles.buttonText}>Adicionar Projeto</Text>
+                </TouchableOpacity>
+
+            </SafeAreaView>
+        </>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#7159c1',
+    },
+    texto: {
+        color: '#ffffff',
+        fontSize: 36,
+    },
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
         justifyContent: 'center',
         alignItems: 'center',
+
     },
-    title: {
-        color: '#ffffff',
-        fontSize: 24,
+    buttonText: {
         fontWeight: 'bold',
+        fontSize: 16,
     }
 });
